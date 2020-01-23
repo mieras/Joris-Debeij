c02007feb8aabf68460071e868fdbb01990d9480
mieras <mieras@gmail.com>
2020-01-23 18:24:36 +0100

    arrow

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,8 @@
-import barba from '@barba/core';
-import barbaCss from '@barba/css';
+// import barba from '@barba/core';
+// import barbaCss from '@barba/css';
 
-// tell Barba to use the css module
-barba.use(barbaCss);
+// // tell Barba to use the css module
+// barba.use(barbaCss);
 
-// init Barba
-barba.init();
+// // init Barba
+// barba.init();

22a75f387c7830b83f6287d471cfdc13a4bcc02d
mieras <mieras@gmail.com>
2020-01-24 10:12:22 +0100

    reveal shit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,21 @@
 
 // // init Barba
 // barba.init();
+
+var slideUp = {
+    distance: '20%',
+    origin: 'bottom',
+    opacity: null,
+    scale: 1.25,
+    opacity: 0
+};
+
+ScrollReveal().reveal('.project', slideUp, );
+
+ScrollReveal().reveal('.project', {
+    delay: 375,
+    duration: 700,
+    reset: true,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    scale: 1
+});

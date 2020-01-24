e3fdae30f9101408ba1649c71744dd4f0b248dcf
mieras <mieras@gmail.com>
2020-01-24 10:15:16 +0100

    no reset

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,7 +20,7 @@ ScrollReveal().reveal('.project', slideUp, );
 ScrollReveal().reveal('.project', {
     delay: 375,
     duration: 700,
-    reset: true,
+    reset: false,
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     scale: 1
 });
